476e03c009efbef5faff775f5d2f5d5f63c63dc0
suffear <122810643+suffear@users.noreply.github.com>
2023-08-10 13:27:12 +0200

    added a pokemonlist array
    for now it contains some data from the 3 starter pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favFood = 'Lasange';
-document.write(favFood);
+let pokelist = [
+  {number: '001', name: 'Bulbasaur', height: "2'04\"", weight: '15.2 lbs', type: ['grass', 'poison']},
+  {number: '004', name: 'Charmander', height: "2'00\"", weight: '18.7 lbs', type: ['fire']},
+  {number: '007', name: 'Squirtle', height: "1'08\"", weight: '19.8 lbs', type: ['water']}
+];
