7dab82a7adf8660a683b8141aeeb0a1d86466fcd
suffear <matzniklas@gmail.com>
2023-08-10 14:05:51 +0200

    sorted the array by number

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 let pokelist = [
-  { number: '001', name: 'Bulbasaur', height: "2'04\"", weight: '15.2 lbs', type: ['grass', 'poison'] },
-  { number: '004', name: 'Charmander', height: "2'00\"", weight: '18.7 lbs', type: ['fire'] },
-  { number: '007', name: 'Squirtle', height: "1'08\"", weight: '19.8 lbs', type: ['water'] },
-  { number: '002', name: 'Ivysaur', height: "3'03\"", weight: '28.7 lbs', type: ['grass', 'poison'] },
-  { number: '003', name: 'Venusaur', height: "6'07\"", weight: '220.5 lbs', type: ['grass', 'poison'] },
-  { number: '005', name: 'Charmeleon', height: "3'07\"", weight: '41.9 lbs', type: ['fire'] },
-  { number: '006', name: 'Charizard', height: "5'07\"", weight: '199.5 lbs', type: ['fire'] },
-  { number: '008', name: 'Wartortle', height: "3'03\"", weight: '49.6 lbs', type: ['water'] },
-  { number: '009', name: 'Blastoise', height: "5'03\"", weight: '188.5 lbs', type: ['water'] }
+  {number: '001', name: 'Bulbasaur', height: "2'04\"", weight: '15.2 lbs', type: ['grass', 'poison']},
+  {number: '002', name: 'Ivysaur', height: "3'03\"", weight: '28.7 lbs', type: ['grass', 'poison']},
+  {number: '003', name: 'Venusaur', height: "6'07\"", weight: '220.5 lbs', type: ['grass', 'poison']},
+  {number: '004', name: 'Charmander', height: "2'00\"", weight: '18.7 lbs', type: ['fire']},
+  {number: '005', name: 'Charmeleon', height: "3'07\"", weight: '41.9 lbs', type: ['fire']},
+  {number: '006', name: 'Charizard', height: "5'07\"", weight: '199.5 lbs', type: ['fire']},
+  {number: '007', name: 'Squirtle', height: "1'08\"", weight: '19.8 lbs', type: ['water']},
+  {number: '008', name: 'Wartortle', height: "3'03\"", weight: '49.6 lbs', type: ['water']},
+  {number: '009', name: 'Blastoise', height: "5'03\"", weight: '188.5 lbs', type: ['water']}
 ];
