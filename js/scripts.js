137a4ccb1e840be62b1d9338db8451edc4e410a2
suffear <matzniklas@gmail.com>
2023-08-22 21:39:19 +0200

    1.7  APIs, Ajax & Asynchronous Behavior + Bonus Task(LoadingMessage)
    - Added fetch polyfills, replaced static array with empty one in "scripts.js".
    - Created functions to fetch data, LoadList() retrieves 151 Pokémon from API.
    - Utilized add() to append Pokémon data to pokemonList, including name and detailsUrl.
    - Implemented loadDetails() to fetch Pokémon details and assign relevant attributes.
    - Consistently assigned LoadList() and loadDetails() in pokemonRepository's returned object.
    - Rendered Pokémon list only after complete data retrieval, enhancing user experience.
    - Executed LoadList() for data fetching, used getAll() to populate list with addListItem()
    - Incorporated loading messages during data fetching to enhance user experience and feedback.

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,83 @@
-let pokeRepo = (function () {
-  let pokeList = [
-    { no: '001', name: 'Bulbasaur', height: [2, 4], weight: 15.2, type: ['grass', 'poison'] },
-    { no: '002', name: 'Ivysaur', height: [3, 3], weight: 28.7, type: ['grass', 'poison'] },
-    { no: '003', name: 'Venusaur', height: [6, 7], weight: 220.5, type: ['grass', 'poison'] },
-    { no: '004', name: 'Charmander', height: [2, 0], weight: 18.7, type: ['fire'] },
-    { no: '005', name: 'Charmeleon', height: [3, 7], weight: 41.9, type: ['fire'] },
-    { no: '006', name: 'Charizard', height: [5, 7], weight: 199.5, type: ['fire'] },
-    { no: '007', name: 'Squirtle', height: [1, 8], weight: 19.8, type: ['water'] },
-    { no: '008', name: 'Wartortle', height: [3, 3], weight: 49.6, type: ['water'] },
-    { no: '009', name: 'Blastoise', height: [5, 3], weight: 188.5, type: ['water'] }
-  ];
-  return {
-    getAll: function () {
-      return pokeList;
-    },
-    add: function (pkmn) {
-      const validate = (value, type, errorMessage) => {
-        if (typeof value !== type) {
-          console.error(errorMessage, value);
-          return false;
-        }
-        return true;
-      };
-    
-      if (
-        validate(pkmn.no, 'string', 'Invalid Pokémon number:') &&
-        validate(pkmn.name, 'string', 'Invalid Pokémon name:') &&
-        validate(pkmn.height, 'object', 'Invalid Pokémon height:') && pkmn.height.length === 2 &&
-        validate(pkmn.height[0], 'number', 'Invalid Pokémon height:') &&
-        validate(pkmn.height[1], 'number', 'Invalid Pokémon height:') &&
-        validate(pkmn.weight, 'number', 'Invalid Pokémon weight:') &&
-        validate(pkmn.type, 'object', 'Invalid Pokémon type:') &&
-        Array.isArray(pkmn.type) &&
-        pkmn.type.every(type => validate(type, 'string', 'Invalid Pokémon type:'))
-      ) {
-        pokeList.push(pkmn);
-      }
-    },
-
-    addListItem: function (pkmn) {
-      let listItem = document.createElement('li');
+const pokeRepo = (function () {
+  let pokeList = [];
+  function showLoadMsg() {
+    const loadMsg = document.createElement('p');
+    loadMsg.classList.add('loadMsg');
+    loadMsg.innerText = 'Loading...';
+    document.body.appendChild(loadMsg);
+  }
+  function hideLoadMsg() {
+    const loadMsg = document.querySelector('body > p');
+    if (loadMsg) {
+      loadMsg.remove();
+    }
+  }
+  async function fetchJSON(url) {
+    const response = await fetch(url);
+    return await response.json();
+  }
+  async function loadList() {
+    showLoadMsg();
+    try {
+      const data = await fetchJSON('https://pokeapi.co/api/v2/pokemon/?limit=150');
+      await Promise.all(data.results.map(async item => {
+        const details = await fetchJSON(item.url);
+        add({
+          name: details.name,
+          detailsUrl: item.url,
+        });
+      }));
+      displayPokeList();
+      hideLoadMsg();
+    } catch (error) {
+      console.error(error);
+      hideLoadMsg();
+    }
+  }
+  async function loadDetails(pkmn) {
+    showLoadMsg();
+    try {
+      const details = await fetchJSON(pkmn.detailsUrl);
+      pkmn.imgUrl = details.sprites.front_default;
+      pkmn.height = details.height;
+      hideLoadMsg();
+    } catch (error) {
+      console.error(error);
+      hideLoadMsg();
+    }
+  }
+  function add(pkmn) {
+    if (typeof pkmn === 'object') {
+      pokeList.push(pkmn);
+    } else {
+      console.error('Invalid Pokémon data format:', pkmn);
+    }
+  }
+  function showDetails(pkmn) {
+    return async function () {
+      await loadDetails(pkmn);
+      console.log(pkmn);
+    };
+  }
+  function caps(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  function displayPokeList() {
+    const list = document.querySelector('.pokeList');
+    pokeList.forEach(pkmn => {
+      const listItem = document.createElement('li');
       listItem.classList.add('pokeItem');
-
-      let button = document.createElement('button');
+      const button = document.createElement('button');
       button.classList.add('pokeBtn');
-      button.innerText = pkmn.name;
-
+      button.innerText = caps(pkmn.name);
+      button.addEventListener('click', showDetails(pkmn));
       listItem.appendChild(button);
-      document.querySelector('.pokeList').appendChild(listItem);
-
-      button.addEventListener('click', () =>  showDetails(pkmn));
-    },
+      list.appendChild(listItem);
+    });
+  }
+  return {
+    add,
+    loadList,
   };
-  
 })();
-
-const showDetails = pokemon => {console.log(pokemon);};
-
-pokeRepo.add({ no: '025', name: 'Pikachu', height: [1, 4], weight: 13.2, type: ['electric'] });
-
-pokeRepo.getAll().forEach(pkmn => {pokeRepo.addListItem(pkmn)})
+pokeRepo.loadList(); 
