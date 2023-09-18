9ca3c0566593ce24f07c0b228f5f4a51111e424e
suffear <matzniklas@gmail.com>
2023-09-18 19:56:19 +0200

    1.8 Creating UI Patterns with JavaScript
    Implement Pokémon Details Modal
    
    - Modified the showDetails() method to display Pokémon details in a modal.
    - Created a modal in the "scripts.js" file to show Pokémon's name, height, and an image.
    - Organized code using IIFEs, comments, and line breaks for better readability.
    - Ensured the modal can be closed via the keyboard.
    - Tested modal interactions on various screen sizes using Chrome DevTools.
    - Dynamically set the image src attribute for responsive and well-positioned images.

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,64 @@
 const pokeRepo = (function () {
   let pokeList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let modalContainer = document.querySelector('#modal-container');
+
   function showLoadMsg() {
     const loadMsg = document.createElement('p');
     loadMsg.classList.add('loadMsg');
     loadMsg.innerText = 'Loading...';
     document.body.appendChild(loadMsg);
   }
+
   function hideLoadMsg() {
     const loadMsg = document.querySelector('body > p');
     if (loadMsg) {
       loadMsg.remove();
     }
   }
+
   async function fetchJSON(url) {
     const response = await fetch(url);
     return await response.json();
   }
+
   async function loadList() {
     showLoadMsg();
     try {
-      const data = await fetchJSON('https://pokeapi.co/api/v2/pokemon/?limit=150');
+      const data = await fetchJSON(apiUrl);
       await Promise.all(data.results.map(async item => {
         const details = await fetchJSON(item.url);
         add({
           name: details.name,
           detailsUrl: item.url,
-          imgUrl: details.sprites.front_default,
+          gifUrl: details.sprites.versions['generation-v']['black-white'].animated['front_default'],
         });
       }));
+      hideLoadMsg();
       displayPokeList();
-      hideLoadMsg();
+      await Promise.all(pokeList.map(loadDetails));
     } catch (error) {
       console.error(error);
       hideLoadMsg();
     }
   }
+
   async function loadDetails(pkmn) {
-    showLoadMsg();
+    let url = pkmn.detailsUrl;
     try {
-      const details = await fetchJSON(pkmn.detailsUrl);
+      const details = await fetchJSON(url);
+      // console.log('details:', details); 
+      pkmn.imgUrl = details.sprites.other['official-artwork'].front_default;
       pkmn.height = details.height;
-      hideLoadMsg();
+      pkmn.weight = details.weight;
+      pkmn.types = details.types;
+      // console.log('pkmn object after fetching details', pkmn); 
     } catch (error) {
       console.error(error);
-      hideLoadMsg();
+      
     }
   }
+
   function add(pkmn) {
     if (typeof pkmn === 'object') {
       pokeList.push(pkmn);
@@ -53,36 +66,84 @@ const pokeRepo = (function () {
       console.error('Invalid Pokémon data format:', pkmn);
     }
   }
-  function showDetails(pkmn) {
-    return async function () {
-      await loadDetails(pkmn);
-      console.log(pkmn);
-    };
-  }
+
   function caps(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
+  
   function displayPokeList() {
     const list = document.querySelector('.pokeList');
     pokeList.forEach(pkmn => {
       const listItem = document.createElement('li');
       listItem.classList.add('pokeItem');
-      const button = document.createElement('button');
-      button.classList.add('pokeBtn');
-      button.innerText = caps(pkmn.name);
+      const btn = document.createElement('btn');
+      btn.classList.add('pokeBtn');
+      btn.innerText = caps(pkmn.name);
       const img = document.createElement('img');
       img.classList.add('pokeImg');
-      img.src = pkmn.imgUrl;
+      img.src = pkmn.gifUrl;
       img.alt = pkmn.name;
-      button.appendChild(img);
-      button.addEventListener('click', showDetails(pkmn));
-      listItem.appendChild(button);
+      btn.appendChild(img);
+      btn.addEventListener('click', () =>  showModal(pkmn));
+      listItem.appendChild(btn);
       list.appendChild(listItem);
     });
   }
+
+// Show details of clicked Pokémon
+  function showModal(pkmn) {
+    
+    // console.log('showModal Called');
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeBtnElement = document.createElement('btn');
+    closeBtnElement.classList.add('modal-close');
+    closeBtnElement.innerText = 'Close';
+    closeBtnElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = pkmn.name;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = 'Height: ' + pkmn.height;
+
+    let imageElement = document.createElement('img');
+    imageElement.src = pkmn.imgUrl;
+    imageElement.alt = pkmn.name;
+
+    modal.appendChild(closeBtnElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modal.appendChild(imageElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal() {
+    modalContainer.classList.remove('is-visible')
+    }
+  
+ window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();  
+    }
+  })  
+  
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  }); 
+
   return {
     add,
     loadList,
   };
 })();
+
 pokeRepo.loadList(); 
